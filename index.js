9ad9271087f6a2c919153c80d33a7820c57ccb07
Evandro Florscuk <evandro_ff@me.com>
2023-07-16 23:27:48 -0300

    Atualização do Projeto => Criação do Boleto router e controller

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const routePlanilha = require('./routes/routePlanilha')
 
 const app = express()
+
+app.use(routePlanilha)
 
 app.listen(8084, _ => {
     console.log('Servidor rodando na porta 8084')
